41107044193d999a03ab88aaf8791a8da6a03e60
edeliscoding <edelreichd@gmail.com>
2024-09-21 22:10:32 -0600

    added comment schema

---
+++ app/models/Comment.js
@@ -0,0 +1,12 @@
+// Comment Schema
+const CommentSchema = new mongoose.Schema({
+  content: { type: String, required: true },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  snippet: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Snippet",
+    required: true,
+  },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
